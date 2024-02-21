31e5b65120b4f528175993ef38d6fece0424548f
FawadLashari <109916922+FawadLashari@users.noreply.github.com>
2024-02-21 19:41:51 +0000

    fifth commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/fairylights sparks.jpf";
+import image from "../images/fairylights sparks.jpg";
 
 const imageAltText = "fairylights sparks in the dark.";
 
